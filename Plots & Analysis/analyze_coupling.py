9b6479c38a78a717cb6dd296193a3373dcab3c74
Likchun <30651770+likchun@users.noreply.github.com>
2021-10-22 02:31:16 +0800

    Update analyze_coupling.py

--- Plots & Analysis/analyze_coupling.py
+++ Plots & Analysis/analyze_coupling.py
@@ -1,30 +1,36 @@
+import os
 from tools import Coupling
 
 
-networks = [('DIV66','\t'), ('RANDOM',' ')]
+networks = [('DIV66','\t'), ('Random',' ')]
 
 for each in networks:
     c = Coupling(each[0]+'.txt', delimiter=each[1], coupling_enhance_factor=1)
     c_calc = c.calculate
     c_plot = c.plot
 
+    print('\n> Displaying details for network \'{}\' <\n'.format(each[0]))
+
     c_plot.AverageSynapticWeight_ExcitatoryIncomingLinks(show_norm=True)
     c_plot.AverageSynapticWeight_InhibitoryIncomingLinks(show_norm=True)
     c_plot.AverageSynapticWeight_OutgoingLinks(show_norm=True)
 
-    coupling_bound = c_calc.CouplingWeightBound()
     connection_probability = c_calc.ConnectionProbability()
-    coupling_stat = c_calc.SynapticWeight_Stat()
-    exc_node_stat = c_calc.ExcitatorySynapticWeight_Stat()
-    inh_node_stat = c_calc.InhibitorySynapticWeight_Stat()
+    synaptic_weight_bound = c_calc.SynapticWeightBounds()
+    synaptic_weight_stat = c_calc.SynapticWeight_Stat()
+    pos_link_stat = c_calc.PositiveSynapticWeight_Stat()
+    neg_link_stat = c_calc.NegativeSynapticWeight_Stat()
 
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), each[0]+'.txt'), 'w') as fp:
-        fp.write('### Statistics of {} Network ###\n\n'.format(each[0]))
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), each[0]+'_stat.txt'), 'w') as fp:
+        fp.write('### Statistics of Network \'{}\' ###\n\n'.format(each[0]))
         fp.write('Connection probability: {}\n'.format(connection_probability))
-        fp.write('Minimum: {}\nMaximum: {}\n'.format(coupling_bound[0], coupling_bound[1]))
-        fp.write('Mean: {}\n'.format(coupling_stat[0]))
-        fp.write('SD: {}\n'.format(coupling_stat[1]))
-        fp.write('Mean (excitatory): {}\n'.format(coupling_stat[0]))
-        fp.write('SD (excitatory): {}\n'.format(exc_node_stat[1]))
-        fp.write('Mean (inhibitory): {}\n'.format(inh_node_stat[0]))
-        fp.write('SD (inhibitory): {}'.format(inh_node_stat[1]))
+        fp.write('\nStatistics of synaptic weights:\n')
+        fp.write('Minimum weight:\t{}\nMaximum weight:\t{}\n'.format(synaptic_weight_bound[0], synaptic_weight_bound[1]))
+        fp.write('Mean (all links):\t{}\n'.format(synaptic_weight_stat[0]))
+        fp.write('S.D. (all links):\t{}\n'.format(synaptic_weight_stat[1]))
+        fp.write('Mean (negative links):\t{}\n'.format(neg_link_stat[0]))
+        fp.write('S.D. (negative links):\t{}\n'.format(neg_link_stat[1]))
+        fp.write('Mean (positive links):\t{}\n'.format(pos_link_stat[0]))
+        fp.write('S.D. (positive links):\t{}'.format(pos_link_stat[1]))
+
+    print('\n------------------------------next------------------------------')

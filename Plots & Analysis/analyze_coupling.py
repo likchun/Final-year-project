19a7f08eda4ef773df4058db6bd05179d1d6c0ba
Likchun <30651770+likchun@users.noreply.github.com>
2021-10-24 01:06:08 +0800

    Update analyze_coupling.py

--- Plots & Analysis/analyze_coupling.py
+++ Plots & Analysis/analyze_coupling.py
@@ -2,18 +2,22 @@ import os
 from tools import Coupling
 
 
-networks = [('DIV66','\t'), ('Random',' ')]
+networks = [
+    {'name':'DIV66',    'delimiter':'\t'},
+    {'name':'Random',   'delimiter':' ' },
+]
 
-for each in networks:
-    c = Coupling(each[0]+'.txt', delimiter=each[1], coupling_enhance_factor=1)
+for network in networks:
+    c = Coupling(network['name']+'.txt', delimiter=network['delimiter'], coupling_enhance_factor=1)
+
     c_calc = c.calculate
     c_plot = c.plot
 
-    print('\n> Displaying details for network \'{}\' <\n'.format(each[0]))
+    print('\n> Displaying details for network \'{}\' <\n'.format(network['name']))
 
-    c_plot.AverageSynapticWeight_ExcitatoryIncomingLinks(show_norm=True)
-    c_plot.AverageSynapticWeight_InhibitoryIncomingLinks(show_norm=True)
-    c_plot.AverageSynapticWeight_OutgoingLinks(show_norm=True)
+    # c_plot.AverageSynapticWeight_ExcitatoryIncomingLinks(show_norm=True)
+    # c_plot.AverageSynapticWeight_InhibitoryIncomingLinks(show_norm=True)
+    # c_plot.AverageSynapticWeight_OutgoingLinks(show_norm=True)
 
     connection_probability = c_calc.ConnectionProbability()
     synaptic_weight_bound = c_calc.SynapticWeightBounds()
@@ -21,8 +25,8 @@ for each in networks:
     pos_link_stat = c_calc.PositiveSynapticWeight_Stat()
     neg_link_stat = c_calc.NegativeSynapticWeight_Stat()
 
-    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), each[0]+'_stat.txt'), 'w') as fp:
-        fp.write('### Statistics of Network \'{}\' ###\n\n'.format(each[0]))
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), network['name']+'_stat.txt'), 'w') as fp:
+        fp.write('### Statistics of Network \'{}\' ###\n\n'.format(network['name']))
         fp.write('Connection probability: {}\n'.format(connection_probability))
         fp.write('\nStatistics of synaptic weights:\n')
         fp.write('Minimum weight:\t{}\nMaximum weight:\t{}\n'.format(synaptic_weight_bound[0], synaptic_weight_bound[1]))

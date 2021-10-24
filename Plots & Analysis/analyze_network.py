9ae4d8d85a90b81e19d706ad16637f8bb13cc5e0
Likchun <30651770+likchun@users.noreply.github.com>
2021-10-24 18:49:12 +0800

    Update and rename analyze_coupling.py to analyze_network.py

--- Plots & Analysis/analyze_network.py
+++ Plots & Analysis/analyze_network.py
@@ -1,5 +1,5 @@
 import os
-from tools import Coupling
+from tools import Network
 
 
 networks = [
@@ -8,22 +8,19 @@ networks = [
 ]
 
 for network in networks:
-    c = Coupling(network['name']+'.txt', delimiter=network['delimiter'], coupling_enhance_factor=1)
-
-    c_calc = c.calculate
-    c_plot = c.plot
+    net = Network(network['name']+'.txt', delimiter=network['delimiter'], coupling_enhance_factor=1)
 
     print('\n> Displaying details for network \'{}\' <\n'.format(network['name']))
 
-    # c_plot.AverageSynapticWeight_ExcitatoryIncomingLinks(show_norm=True)
-    # c_plot.AverageSynapticWeight_InhibitoryIncomingLinks(show_norm=True)
-    # c_plot.AverageSynapticWeight_OutgoingLinks(show_norm=True)
+    # net.plot.AverageSynapticWeight_ExcitatoryIncomingLinks(show_norm=True)
+    # net.plot.AverageSynapticWeight_InhibitoryIncomingLinks(show_norm=True)
+    # net.plot.AverageSynapticWeight_OutgoingLinks(show_norm=True)
 
-    connection_probability = c_calc.ConnectionProbability()
-    synaptic_weight_bound = c_calc.SynapticWeightBounds()
-    synaptic_weight_stat = c_calc.SynapticWeight_Stat()
-    pos_link_stat = c_calc.PositiveSynapticWeight_Stat()
-    neg_link_stat = c_calc.NegativeSynapticWeight_Stat()
+    connection_probability = net.calculate.ConnectionProbability()
+    synaptic_weight_bound = net.calculate.SynapticWeightBounds()
+    synaptic_weight_stat = net.calculate.SynapticWeight_Stat()
+    pos_link_stat = net.calculate.PositiveSynapticWeight_Stat()
+    neg_link_stat = net.calculate.NegativeSynapticWeight_Stat()
 
     with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), network['name']+'_stat.txt'), 'w') as fp:
         fp.write('### Statistics of Network \'{}\' ###\n\n'.format(network['name']))
